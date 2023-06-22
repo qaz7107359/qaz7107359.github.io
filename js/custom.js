efed1db372854f0dc418522d33262de804dbbddf
qaz7107359 <774188964@qq.com>
2023-06-23 00:24:29 +0800

    Site updated: 2023-06-23 00:24:27

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+custom_js:
+  - /js/diy/timeDate.js
+  - //cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js  #/APlayer#/APlayer依赖
+  - //cdn.jsdelivr.net/gh/metowolf/Metingjs@1.2/dist/Meting.min.js  #/APlayer依赖
+# 指定自定义 .css 文件路径，用法和 custom_js 相同
+# The usage is the same as custom_js
+
